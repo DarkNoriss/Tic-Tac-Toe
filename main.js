12c527d6c51c3443c74040e3bec33d418850d43a
DarkNoriss <darknoriss@gmail.com>
2023-03-17 16:52:23 +0100

    added basic code (html, css and draw board script)

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 import "./style.scss";
+
+const appContainer = document.querySelector(".app");
+
+window.onload = function () {
+  createBoard();
+};
+
+const gameboard = () => {
+  let board = new Array(9);
+};
+
+function createBoard() {
+  for (let start = 0; start < 9; start++) {
+    const newBoard = document.createElement("div");
+    newBoard.classList.add("board");
+    newBoard.setAttribute("id", start);
+    newBoard.addEventListener("click", makeMove);
+
+    appContainer.appendChild(newBoard);
+  }
+}
+
+function makeMove(event) {
+  console.log("yey i can click!");
+}
